bdbd2ba17e4e5222e77cbc344c440a0434531e38
unknown <eb30780@seeu.edu.mk>
2026-03-30 12:32:10 +0200

    Converted Incremental Load into stored procedure

--- SQL_Load_Methods/incremental_load.sql
+++ SQL_Load_Methods/incremental_load.sql
@@ -1,3 +1,7 @@
-INSERT INTO target_table
-SELECT * FROM source_table
-WHERE id NOT IN (SELECT id FROM target_table);
+CREATE PROCEDURE Incremental_Load
+AS
+BEGIN
+    INSERT INTO target_table
+    SELECT * FROM source_table
+    WHERE id NOT IN (SELECT id FROM target_table);
+END
