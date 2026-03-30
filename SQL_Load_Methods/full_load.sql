5eedabb96099f537ed89abf01522a13c5e7cf403
unknown <eb30780@seeu.edu.mk>
2026-03-30 12:25:51 +0200

    Initial Full_Load with basic insert logic

--- SQL_Load_Methods/full_load.sql
+++ SQL_Load_Methods/full_load.sql
@@ -1,7 +1,2 @@
-CREATE PROCEDURE Full_Load (@source NVARCHAR(100), @target NVARCHAR(100))
-AS
-BEGIN
-    DECLARE @sql NVARCHAR(MAX)
-    SET @sql = 'INSERT INTO ' + @target + ' SELECT * FROM ' + @source
-    EXEC(@sql)
-END
+INSERT INTO target_table
+SELECT * FROM source_table;
