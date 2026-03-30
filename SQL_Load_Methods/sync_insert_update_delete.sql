d3109ef735a78f4e64e2c1d23c5cb6f80557824b
unknown <eb30780@seeu.edu.mk>
2026-03-30 11:08:12 +0200

    Enhanced Sync with dynamic parameters

--- SQL_Load_Methods/sync_insert_update_delete.sql
+++ SQL_Load_Methods/sync_insert_update_delete.sql
@@ -1,9 +1,18 @@
-MERGE target_table AS T
-USING source_table AS S
-ON T.id = S.id
-WHEN MATCHED THEN
-    UPDATE SET T.name = S.name
-WHEN NOT MATCHED THEN
-    INSERT (id, name) VALUES (S.id, S.name)
-WHEN NOT MATCHED BY SOURCE THEN
-    DELETE;
+CREATE PROCEDURE Sync_Data (@source NVARCHAR(100), @target NVARCHAR(100))
+AS
+BEGIN
+    DECLARE @sql NVARCHAR(MAX)
+
+    SET @sql = '
+    MERGE ' + @target + ' AS T
+    USING ' + @source + ' AS S
+    ON T.id = S.id
+    WHEN MATCHED THEN
+        UPDATE SET T.name = S.name
+    WHEN NOT MATCHED THEN
+        INSERT (id, name) VALUES (S.id, S.name)
+    WHEN NOT MATCHED BY SOURCE THEN
+        DELETE;'
+
+    EXEC(@sql)
+END
