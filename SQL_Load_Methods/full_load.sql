6a41775417b6700b2c5257c067c3ab119938bd25
unknown <eb30780@seeu.edu.mk>
2026-03-30 10:40:21 +0200

    Enhanced Full_Load with dynamic parameters

--- SQL_Load_Methods/full_load.sql
+++ SQL_Load_Methods/full_load.sql
@@ -1,2 +1,7 @@
-INSERT INTO target_table
-SELECT * FROM source_table;
+CREATE PROCEDURE Full_Load (@source NVARCHAR(100), @target NVARCHAR(100))
+AS
+BEGIN
+    DECLARE @sql NVARCHAR(MAX)
+    SET @sql = 'INSERT INTO ' + @target + ' SELECT * FROM ' + @source
+    EXEC(@sql)
+END
