a1210e07eb1d8a0304672afe3966e6e139dea6e5
unknown <eb30780@seeu.edu.mk>
2026-03-30 11:01:46 +0200

    Enhanced Append with dynamic parameters

--- SQL_Load_Methods/Append.sql
+++ SQL_Load_Methods/Append.sql
@@ -1,2 +1,11 @@
-INSERT INTO target_table (id, name)
-SELECT id, name FROM source_table;
+CREATE PROCEDURE Append_Data (@source NVARCHAR(100), @target NVARCHAR(100))
+AS
+BEGIN
+    DECLARE @sql NVARCHAR(MAX)
+
+    SET @sql = '
+    INSERT INTO ' + @target + ' (id, name)
+    SELECT id, name FROM ' + @source
+
+    EXEC(@sql)
+END
