9499c4d7e5a60e067f456eb762ab06edc76167f6
unknown <eb30780@seeu.edu.mk>
2026-03-30 12:29:38 +0200

    Converted Upsert into stored procedure

--- SQL_Load_Methods/upsert.sql
+++ SQL_Load_Methods/upsert.sql
@@ -1,7 +1,11 @@
-MERGE target_table AS T
-USING source_table AS S
-ON T.id = S.id
-WHEN MATCHED THEN
-    UPDATE SET T.name = S.name
-WHEN NOT MATCHED THEN
-    INSERT (id, name) VALUES (S.id, S.name);
+CREATE PROCEDURE Upsert_Data
+AS
+BEGIN
+    MERGE target_table AS T
+    USING source_table AS S
+    ON T.id = S.id
+    WHEN MATCHED THEN
+        UPDATE SET T.name = S.name
+    WHEN NOT MATCHED THEN
+        INSERT (id, name) VALUES (S.id, S.name);
+END
