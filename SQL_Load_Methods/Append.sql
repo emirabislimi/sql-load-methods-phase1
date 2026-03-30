0a7a7acf869c3c50cc68c27cfa9058f131e53c58
unknown <eb30780@seeu.edu.mk>
2026-03-30 12:30:36 +0200

    Initial Append logic

--- SQL_Load_Methods/Append.sql
+++ SQL_Load_Methods/Append.sql
@@ -1,11 +1,2 @@
-CREATE PROCEDURE Append_Data (@source NVARCHAR(100), @target NVARCHAR(100))
-AS
-BEGIN
-    DECLARE @sql NVARCHAR(MAX)
-
-    SET @sql = '
-    INSERT INTO ' + @target + ' (id, name)
-    SELECT id, name FROM ' + @source
-
-    EXEC(@sql)
-END
+INSERT INTO target_table (id, name)
+SELECT id, name FROM source_table;
