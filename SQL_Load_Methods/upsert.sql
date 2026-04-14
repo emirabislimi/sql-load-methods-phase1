6b1283d4ef32516a668dc0df7649648ca47fd0f4
unknown <eb30780@seeu.edu.mk>
2026-04-14 09:45:26 +0200

    Add basic Upsert (update all + insert)

--- SQL_Load_Methods/upsert.sql
+++ SQL_Load_Methods/upsert.sql
@@ -1,9 +1,10 @@
+-- UPSERT (BASIC)
+
 CREATE OR ALTER PROCEDURE Staging.Upsert_Students
 AS
 BEGIN
     BEGIN TRY
 
-        -- UPDATE existing (only changed)
         UPDATE S
         SET 
             S.Name = L.Name,
@@ -11,22 +12,15 @@ BEGIN
             S.UpdatedAt = L.UpdatedAt
         FROM Staging.Students S
         INNER JOIN Landing.Students L
-            ON S.Id = L.Id
-        WHERE 
-            S.Name <> L.Name
-            OR S.Age <> L.Age
-            OR S.UpdatedAt <> L.UpdatedAt;
+            ON S.Id = L.Id;
+        -- basic: updates all rows
 
-        -- INSERT NEW
         INSERT INTO Staging.Students (Id, Name, Age, UpdatedAt)
         SELECT L.Id, L.Name, L.Age, L.UpdatedAt
         FROM Landing.Students L
         LEFT JOIN Staging.Students S
             ON L.Id = S.Id
         WHERE S.Id IS NULL;
-
-        INSERT INTO Audit.Logs (ProcedureName, Status, Message)
-        VALUES ('Upsert_Students', 'SUCCESS', 'Upsert completed');
 
     END TRY
     BEGIN CATCH
